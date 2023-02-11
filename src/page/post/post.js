1f46b3cd58f2cfd06dcbd56777607c779ae4cbb2
Abdelouahab <ad.dev.main@gmail.com>
2023-02-11 17:57:50 +0100

    add and remove tuples based on key

--- src/page/post/post.js
+++ src/page/post/post.js
@@ -1,90 +1,10 @@
 import "./post.css";
-import "./contentStyles.css";
-import React, { useState, useEffect, useRef } from "react";
-import { v4 as uuidv4 } from "uuid";
+import React from "react";
 
 import Header from "../../component/header";
+import Edit from "./edit";
 
 import editIcon from "../../asset/image/editIcon.svg";
-import dragAndDropIcon from "../../asset/image/drag.svg";
-import optionsIcon from "../../asset/image/options.svg";
-
-const Tuple = (props) => {
-  const [text, setText] = useState("ajgklfahptrgqhnjytgjeiawq");
-  function handleKeyInput(e) {
-    if (e.key === "Enter") {
-      e.preventDefault();
-      props.addTuple(e.currentTarget.id);
-    }
-
-    if ((e.key === "Delete" || e.key === "Backspace") && text === "") {
-      props.deleteTuple(e.currentTarget.id);
-    }
-  }
-
-  return (
-    <div className="tuple">
-      <div className="options">
-        <img src={dragAndDropIcon} alt="" />
-        <img src={optionsIcon} alt="" />
-      </div>
-
-      <textarea
-        className="default"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-        onKeyDown={handleKeyInput}
-        id={props.id}
-        ref={props.inputRef}
-      />
-    </div>
-  );
-};
-const Edit = () => {
-  const [key, setKey] = useState(uuidv4());
-  const [article, setArticle] = useState([
-    <Tuple addTuple={addTuple} deleteTuple={deleteTuple} key={key} id={key} />,
-  ]);
-
-  useEffect(() => {
-    if (document.getElementById(key)) document.getElementById(key).focus();
-  }, [article, key]);
-
-  function addTuple(id) {
-    const newKey = uuidv4();
-    setArticle((prevArticle) => {
-      const index = prevArticle.findIndex((tuple) => tuple.key === id);
-      return [
-        ...prevArticle.slice(0, index + 1),
-        <Tuple
-          addTuple={addTuple}
-          deleteTuple={deleteTuple}
-          key={newKey}
-          id={newKey}
-        />,
-        ...prevArticle.slice(index + 1),
-      ];
-    });
-    setKey(newKey);
-  }
-  function deleteTuple(id) {
-    setArticle((prevArticle) => {
-      const prevTupleIndex =
-        prevArticle.findIndex((tuple) => tuple.key === id) - 1;
-
-      if (prevTupleIndex < 0) return prevArticle;
-      setKey(prevArticle[prevTupleIndex].key);
-      return prevArticle.filter((tuple) => tuple.key !== id);
-    });
-  }
-  return (
-    <div className="article">
-      <textarea name="title" placeholder="TITLE" className="title" height="" />
-      <div className="line"></div>
-      <div className="tuples">{article}</div>
-    </div>
-  );
-};
 
 const Post = (props) => {
   return (
---
+++ src/page/post/tuple.js
@@ -0,0 +1,54 @@
+import React from "react";
+import "./tuple.css";
+
+import dragAndDropIcon from "../../asset/image/drag.svg";
+import optionsIcon from "../../asset/image/options.svg";
+
+const Tuple = ({
+  addTuple,
+  deleteTuple,
+  tuple,
+  isEditable,
+  saveTextStateChange,
+}) => {
+  function handleKeyInput(e) {
+    console.log(e.key);
+    if (e.key === "Enter") {
+      e.preventDefault();
+      addTuple(e.currentTarget.id);
+      return;
+    }
+    if (
+      (e.key === "Delete" || e.key === "Backspace") &&
+      e.currentTarget.textContent === ""
+    ) {
+      deleteTuple(e.currentTarget.id);
+      return;
+    }
+    if (e.key === "Tab") {
+    }
+  }
+  return (
+    <div className="tuple">
+      <div className="options">
+        <img src={dragAndDropIcon} alt="" />
+        <img src={optionsIcon} alt="" />
+      </div>
+      <div className="tupleContent">
+        {React.createElement(
+          tuple.type,
+          {
+            id: tuple.id,
+            tabIndex: "0",
+            onKeyDown: handleKeyInput,
+            onFocus: isEditable,
+            onBlur: saveTextStateChange,
+          },
+          tuple.value
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Tuple;
